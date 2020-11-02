2ecaf4f075c5b10d64fff53c3efb433067a3f820
Vincent Chapront <vincent.chapront@gmail.com>
2020-11-02 12:03:40 +0100

    Utilisation de JSX

--- src/Components/membre.js
+++ src/Components/membre.js
@@ -2,11 +2,16 @@ import React, { Component } from 'react';
 
 class AppBis extends Component{
     render() {
-        return React.createElement(
-            'div',
-            null,
-            React.createElement('h1',null,"Test TITRE")
-        );
+        // return React.createElement(
+        //     'div',
+        //     null,
+        //     React.createElement('h1',null,"Test TITRE")
+        // );
+        return (
+            <div>
+                <h1>Test TITRE2</h1>
+            </div>
+        )
     }
 }
 
