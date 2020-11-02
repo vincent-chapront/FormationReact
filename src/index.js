8f8a74001c0e3c8da5716a5b15fcb4b373c70361
Vincent Chapront <vincent.chapront@gmail.com>
2020-11-02 12:06:40 +0100

    Réutilisation du composant

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import AppBis from './Components/membre';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+        <AppBis />
   </React.StrictMode>,
   document.getElementById('root')
 );
