89b5ea02b2bfaae83cbc99521f13dc0abec059eb
Vincent Chapront <vincent.chapront@gmail.com>
2020-11-02 12:22:53 +0100

    Balise Fragment, pour pouvoir "return" plusieurs balise au premier niveau

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
 import AppBis from './Components/membre';
+import { Fragment } from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <AppBis />
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <div className="App">
+        <header className="App-header">
+          <AppBis />
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+      <AppBis />
+    </Fragment>
   );
 }
 
