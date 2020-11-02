7189dc98f617d775139eb248b5196175833d9895
Vincent Chapront <vincent.chapront@gmail.com>
2020-11-02 11:51:22 +0100

    Creation composant

---
+++ src/Components/membre.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class AppBis extends Component{
+    render() {
+        return React.createElement(
+            'div',
+            null,
+            React.createElement('h1',null,"Test TITRE")
+        );
+    }
+}
+
+export default AppBis
