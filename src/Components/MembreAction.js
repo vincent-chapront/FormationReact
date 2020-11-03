8dd5442beb507e8cc0a8a319843d327368724db8
Vincent Chapront <vincent.chapront@gmail.com>
2020-11-03 14:30:07 +0100

    Affichage

--- src/Components/MembreAction.js
+++ src/Components/MembreAction.js
@@ -9,12 +9,20 @@ class MembreAction extends Component{
         const {membre, handlerAge, handlerName,incr}=this.props
         return (
             <Fragment >
-              <Membre name={membre.name} age={membre.age}/>
-              <input type="text" onChange={event=>handlerName(membre,event.target.value)} value={membre.name}></input>
-              <p>              
-              <Button className="btn-danger" vieillir={()=>handlerAge(membre,-incr)} text={"-"+incr}/>
-              <Button className="btn-success" vieillir={()=>handlerAge(membre,+incr)} text={"+"+incr}/>
-              </p>
+               <div class="container">
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <Membre name={membre.name} age={membre.age}/>
+                        </div>
+                        <div class="col-sm-3">
+                            <input type="text" onChange={event=>handlerName(membre,event.target.value)} value={membre.name}></input>
+                        </div>
+                        <div class="col-sm-3">
+                            <Button className="btn-danger" vieillir={()=>handlerAge(membre,-incr)} text={"-"+incr}/>
+                            <Button className="btn-success" vieillir={()=>handlerAge(membre,+incr)} text={"+"+incr}/>
+                        </div>
+                    </div>
+                </div>
             </Fragment>
         )
     }
