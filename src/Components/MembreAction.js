756f6e82787d00dafedac97671987b58a5aefe1b
Vincent Chapront <vincent.chapront@gmail.com>
2020-11-03 15:15:59 +0100

    Correction class=>className

--- src/Components/MembreAction.js
+++ src/Components/MembreAction.js
@@ -9,15 +9,15 @@ class MembreAction extends Component{
         const {membre, handlerAge, handlerName,incr}=this.props
         return (
             <Fragment >
-               <div class="container">
-                    <div class="row">
-                        <div class="col-sm-6">
+               <div className="container">
+                    <div className="row">
+                        <div className="col-sm-6">
                             <Membre name={membre.name} age={membre.age}/>
                         </div>
-                        <div class="col-sm-3">
+                        <div className="col-sm-3">
                             <input type="text" onChange={event=>handlerName(membre,event.target.value)} value={membre.name}></input>
                         </div>
-                        <div class="col-sm-3">
+                        <div className="col-sm-3">
                             <Button className="btn-danger" vieillir={()=>handlerAge(membre,-incr)} text={"-"+incr}/>
                             <Button className="btn-success" vieillir={()=>handlerAge(membre,+incr)} text={"+"+incr}/>
                         </div>
