b11d31deff6635bacd7ec81bcc23c2002912b19f
Vincent Chapront <vincent.chapront@gmail.com>
2020-11-03 15:36:31 +0100

    Utilisation d'un handler unique pour les input du formulaire

--- src/Components/Formulaire.js
+++ src/Components/Formulaire.js
@@ -2,15 +2,10 @@ import React, { Component, Fragment } from 'react';
 
 class Formulaire extends Component{
     state={name:"Zacary",age:17}
-    changeName=event=>{
-        const state=this.state
-        state.name=event.target.value
-        this.setState(state)
+    change=event=>{
+        const { name , value }= event.target
+        this.setState({ [name]:value})
     }
-    changeAge=event=>{
-        const state=this.state
-        state.age=event.target.value
-        this.setState(state)
     }
 
     render() {
@@ -20,13 +15,14 @@ class Formulaire extends Component{
                 <div className="container">
                     <div className="row">
                         <div className="col-sm-6">
-                            <input type="text" placeholder="Nom" value={this.state.name}
-                                onChange={this.changeName}
+                            <input type="text" name="name" placeholder="Nom" value={this.state.name}
+                                onChange={this.change}
                             ></input>
                         </div>
                         <div className="col-sm-3">
-                            <input type="number" min="0" placeholder="Age"value={this.state.age}
-                                onChange={this.changeAge}></input>
+                            <input type="number" name="age" min="0" placeholder="Age"value={this.state.age}
+                                onChange={this.change}
+                                ></input>
                         </div>
                         <div className="col-sm-3">
                             <button onClick={()=>handleSubmit(this.state.name, this.state.age)}>Save</button>
